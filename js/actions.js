768a5d7b8ea50563c686487a12f5a566dd38a30e
henriquetroiano <henriquetroiano@hotmail.com>
2020-07-07 20:21:07 -0300

    last commit

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,9 @@
 const anchor = document.querySelectorAll('a')
-
+console.log(anchor)
 anchor.forEach((a) => {
-    
+    a.addEventListener('click', function(event) {
+        event.preventDefault();
+        const thisSection = document.querySelectorAll(a.hash)
+        window.scrollTo(0, thisSection[0].offsetTop - 100)
+    })
 })
